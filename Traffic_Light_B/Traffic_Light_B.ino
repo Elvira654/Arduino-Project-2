c450c617b256cfd8104a1f989fcdea63edb8fd75
Elvira <57684716+Elvira654@users.noreply.github.com>
2021-08-02 14:10:58 +0200

    Add files via upload

---
+++ Traffic_Light_B/Traffic_Light_B.ino
@@ -0,0 +1,65 @@
+int crveno=10;
+int zuto=9;
+int zeleno=8;
+
+int crveno_Za_Pjesake=7;
+int zeleno_Za_Pjesake=6;
+
+void setup()
+{
+  pinMode(crveno, OUTPUT);
+  pinMode(zuto, OUTPUT);
+  pinMode(zeleno, OUTPUT);
+
+  pinMode(crveno_Za_Pjesake,OUTPUT);
+  pinMode(zeleno_Za_Pjesake,OUTPUT);
+
+}
+void loop()
+{
+  digitalWrite(crveno,HIGH);
+  digitalWrite(zeleno_Za_Pjesake,HIGH);
+  delay(5000);
+  digitalWrite(zeleno_Za_Pjesake,LOW);
+  delay(1000);
+  digitalWrite(zeleno_Za_Pjesake,HIGH);
+  delay(1000);
+  digitalWrite(zeleno_Za_Pjesake,LOW);
+  digitalWrite(crveno_Za_Pjesake,HIGH);
+  digitalWrite(zuto,HIGH);
+  delay(1000);
+  digitalWrite(zuto,LOW);
+  digitalWrite(crveno,LOW);
+  digitalWrite(zeleno,HIGH);
+  delay(8000);
+  digitalWrite(zeleno,LOW);
+  delay(1000);
+  digitalWrite(zeleno,HIGH);
+  delay(1000);
+  digitalWrite(zeleno,LOW);
+  digitalWrite(zuto,HIGH);
+  delay(2000);
+  digitalWrite(zuto,LOW);
+  digitalWrite(crveno,HIGH);
+  digitalWrite(crveno_Za_Pjesake,LOW);
+  digitalWrite(zeleno_Za_Pjesake,HIGH);
+  delay(5000);
+  digitalWrite(zeleno_Za_Pjesake,LOW);
+  delay(1000);
+  digitalWrite(zeleno_Za_Pjesake,HIGH);
+  delay(1000);
+  digitalWrite(zeleno_Za_Pjesake,LOW);
+  digitalWrite(crveno_Za_Pjesake,HIGH);
+  digitalWrite(zuto,HIGH);
+  delay(1000);
+  digitalWrite(crveno,LOW);
+  digitalWrite(zuto,LOW);
+  digitalWrite(zeleno,HIGH);
+  delay(6000);
+  digitalWrite(zeleno,LOW);
+  delay(1000);
+  digitalWrite(zeleno,HIGH);
+  delay(1000);
+  digitalWrite(zeleno,LOW);
+  digitalWrite(zuto,HIGH);
+}
